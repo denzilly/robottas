3309d9c707897e69c7ad950b074674e46533678e
denzilly <btimmer313@gmail.com>
2020-07-12 00:36:45 +0200

    added linux support

--- modules/create_webdriver.py
+++ modules/create_webdriver.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-
+import platform
 
 def create_webdriver():
     options = Options()
     options.headless = True
 
-    driver = webdriver.Firefox(executable_path='modules/data/resources/geckodriver.exe', options = options)
+
+    #select correct geckodriver for your system
+    if "Windows" in platform.system():
+        driver = webdriver.Firefox(executable_path='modules/data/resources/geckodriver.exe', options = options)
+    elif "Linux" in platform.system():
+        driver = webdriver.Firefox(executable_path='modules/data/resources/geckodriver', options=options)
 
 
     return driver
